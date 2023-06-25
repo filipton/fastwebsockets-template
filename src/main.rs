f6bd63c9ed48bd0b3892c8dd5643f431f1d1b14c
filipton <filipton12@gmail.com>
2023-06-26 00:26:36 +0200

    feat: working broadcast

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,65 @@
+use crate::structs::WsMessage;
 use fastwebsockets::upgrade::upgrade;
 use fastwebsockets::{FragmentCollector, OpCode};
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
 use hyper::upgrade::Upgraded;
 use hyper::{Body, Request, Response};
+use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::sync::Arc;
+use structs::State;
 use tokio::net::TcpListener;
+use tokio::sync::Mutex;
+
+mod structs;
 
 async fn handle_ws(
     mut ws: FragmentCollector<Upgraded>,
+    client_addr: SocketAddr,
+    state: Arc<Mutex<State>>,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+    {
+        let mut state = state.lock().await;
+        state.clients.insert(client_addr, tx);
+    }
+
     loop {
-        let frame = ws.read_frame().await?;
-        match frame.opcode {
-            OpCode::Close => {
-                //println!("Closing connection...");
-                break;
+        tokio::select! {
+            frame = ws.read_frame() => {
+                let frame = frame?;
+
+                match frame.opcode {
+                    OpCode::Close => {
+                        //println!("Closing connection...");
+                        break;
+                    }
+                    OpCode::Text => {
+                        let text = String::from_utf8(frame.payload.to_vec()).unwrap();
+                        state.lock().await.broadcast(&client_addr, WsMessage::Text(text)).await;
+                        //ws.write_frame(frame).await?;
+                    }
+                    OpCode::Binary => {
+                        state.lock().await.broadcast(&client_addr, WsMessage::Binary(frame.payload.to_vec())).await;
+                        //ws.write_frame(frame).await?;
+                    }
+                    _ => {}
+                }
+            },
+            frame = rx.recv() => {
+                if let Some(frame) = frame {
+                    ws.write_frame(frame.to_frame()).await?;
+                } else {
+                    break;
+                }
             }
-            OpCode::Text | OpCode::Binary => {
-                ws.write_frame(frame).await?;
-            }
-            _ => {}
         }
+    }
+
+    {
+        let mut state = state.lock().await;
+        state.clients.remove(&client_addr);
     }
 
     Ok(())
@@ -29,6 +67,8 @@ async fn handle_ws(
 
 async fn request_handler(
     mut req: Request<Body>,
+    client_addr: SocketAddr,
+    state: Arc<Mutex<State>>,
 ) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
     let uri = req.uri().to_string();
     let uri = uri.as_str();
@@ -39,7 +79,7 @@ async fn request_handler(
 
             tokio::spawn(async move {
                 let ws = fastwebsockets::FragmentCollector::new(fut.await.unwrap());
-                handle_ws(ws).await.unwrap();
+                handle_ws(ws, client_addr, state).await.unwrap();
             });
 
             return Ok(response);
@@ -56,12 +96,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 1234));
     let listener = TcpListener::bind(addr).await?;
 
+    let state = Arc::new(Mutex::new(State {
+        clients: HashMap::new(),
+    }));
+
     loop {
-        let (stream, _) = listener.accept().await?;
+        let (stream, client_addr) = listener.accept().await?;
+        let state = state.clone();
 
         tokio::task::spawn(async move {
             if let Err(err) = Http::new()
-                .serve_connection(stream, service_fn(move |req| request_handler(req)))
+                .serve_connection(
+                    stream,
+                    service_fn(move |req| request_handler(req, client_addr, state.clone())),
+                )
                 .with_upgrades()
                 .await
             {
@@ -70,4 +118,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         });
     }
 }
-
---
+++ src/structs.rs
@@ -0,0 +1,45 @@
+use std::{net::SocketAddr, collections::HashMap};
+
+use fastwebsockets::Frame;
+use tokio::sync::mpsc::UnboundedSender;
+
+#[derive(Clone, Debug)]
+pub enum WsMessage {
+    Text(String),
+    Binary(Vec<u8>),
+
+    /// Send a pong message with the given data.
+    Pong(Vec<u8>),
+
+    /// Close the connection with the given code and reason.
+    ///
+    /// u16 is the status code
+    /// String is the reason
+    Close(u16, String),
+}
+
+impl WsMessage {
+    pub fn to_frame(&self) -> Frame {
+        match self {
+            WsMessage::Text(text) => Frame::text(text.as_bytes().into()),
+            WsMessage::Binary(data) => Frame::binary(data.as_slice().into()),
+            WsMessage::Pong(data) => Frame::pong(data.as_slice().into()),
+            WsMessage::Close(code, reason) => Frame::close(*code, reason.as_bytes()),
+        }
+    }
+}
+
+type Tx = UnboundedSender<WsMessage>;
+pub struct State {
+    pub clients: HashMap<SocketAddr, Tx>,
+}
+
+impl State {
+    pub async fn broadcast(&self, sender: &SocketAddr, msg: WsMessage) {
+        for (addr, tx) in self.clients.iter() {
+            if addr != sender {
+                tx.send(msg.clone()).unwrap();
+            }
+        }
+    }
+}
